da731ec9d432a9c440c770945d33ad74efd30dff
Marinus Swanepoel <marinus.swanepoel@tworedkites.com>
2019-04-08 14:17:44 +1000

    Move existing functionality into seperate modules

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-const counts = [1, 2, 3];
-const fills = ['Empty', 'Partial', 'Solid'];
-const glyphShapes = ['Diamond', 'Tilde', 'Oval'];
-const colors = ['Red', 'Green', 'Purple'];
-const cardDimensions = {
-  counts, glyphShapes, colors, fills,
-};
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-function reduceCallback(acc, dimension) {
-  const cards = dimension.values.map((value) => (
-    acc.map((existingCard) => (
-      {[dimension.name]: value, ...existingCard}
-    ))
-  ));
-  return [].concat(...cards);
-}
-
-function generateDeck() {
-  return Object.keys(cardDimensions).map((dimensionName) => (
-    {
-      name: dimensionName,
-      values: cardDimensions[dimensionName],
-    }
-  )).reduce(reduceCallback, [{}]);
-}
+const shuffle = require('lodash.shuffle');
+const generateDeck = require('./lib/deck');
 
 const cards = generateDeck();
+const sample = shuffle(cards).slice(0, 12);
 
-debugger
-console.log(cards);
+console.log(sample);
 
